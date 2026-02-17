fd8669ac31ad231883fe7f93a4595c44139167a9
SwethaNeela <neelaswetha20@gmail.com>
2026-02-17 15:26:47 +0530

    lambda chnages

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,42 +1,16 @@
-import json
-
-# import requests
-
+import boto3
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
-
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
-
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
-        }),
-    }
+    s3_object = event['Records'][0]['s3']['object']['key']
+    message = f"A new image named {s3_object} has been uploaded."
+    # Send email via SES
+    ses = boto3.client('ses')
+    ses.send_email(
+        Source='neelaswetha20@gmail.com',
+        Destination={'ToAddresses': ['neelaswetha20@gmail.com']},
+        Message={
+            'Subject': {'Data': 'New Image Uploaded'},
+            'Body': {'Text': {'Data': message}}
+        }
+    )
+    return {"status": "success"}
